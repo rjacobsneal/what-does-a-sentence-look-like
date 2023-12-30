1c58cfa0750c6668a8d81b85d75c850e24af8c71
rjacobsneal <reese.neal@yale.edu>
2023-12-29 19:38:09 -0600

    animations, etc.

---
+++ public/script.js
@@ -0,0 +1,44 @@
+const numberOfSquares = 10;
+const animationDuration = 8; // seconds
+
+for (let i = 0; i < numberOfSquares; i++) {
+  createFloatingSquare(i);
+}
+
+function createFloatingSquare(i) {
+  const colors = [
+    "#87bfc9",
+    "#a9d8db",
+    "#d5ebf1",
+    "#f4ebd1",
+    "#f6c788",
+    "#fab098",
+    "#9ec6ff",
+    "#b599e1",
+    "#e1d9f4",
+    "#fcd994",
+    "#ed655f",
+  ];
+
+  const element = document.createElement("div");
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  const squareSize = Math.min(window.innerWidth, window.innerHeight) * 0.09;
+
+  element.className = "square";
+  element.style.position = "absolute";
+  element.style.zIndex = "-1";
+  element.style.backgroundColor = randomColor;
+  element.style.width = `${squareSize}px`;
+  element.style.height = `${squareSize}px`;
+
+  // Randomly position each square on the screen
+  const x = i * (window.innerWidth / numberOfSquares);
+  const y = window.innerHeight + squareSize;
+  element.style.left = `${x}px`;
+  element.style.top = `${y}px`;
+
+  const animationDelay = Math.random() * animationDuration;
+  element.style.animation = `floatAnimation ${animationDuration}s linear ${animationDelay}s infinite`;
+
+  document.body.appendChild(element);
+}
