fdfecc99a4ba1d6d5b16f9294560315375349453
rjacobsneal <reese.neal@yale.edu>
2023-12-31 14:47:00 -0600

    prevent default scrolling with spacebar

--- public/helper.js
+++ public/helper.js
@@ -1,3 +1,9 @@
+window.addEventListener("keydown", function (e) {
+  if (e.key === " " && e.target === document.body) {
+    e.preventDefault();
+  }
+});
+
 function redactInput(input) {
   const placeholderChar = "*";
   const inputValue = input.value;
